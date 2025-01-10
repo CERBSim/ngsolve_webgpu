75b285abea01a8eee845b501f46caa7f2e547200
Christopher Lackner <clackner@cerbsim.com>
2025-01-10 09:35:44 +0100

    add new working cells in juypter notebook

--- ngsolve_webgpu/isosurface.py
+++ ngsolve_webgpu/isosurface.py
@@ -1,4 +1,3 @@
-
 import webgpu
 from webgpu.gpu import RenderObject
 from webgpu.utils import (
@@ -8,7 +7,7 @@ from webgpu.utils import (
     create_bind_group,
     decode_bytes,
     encode_bytes,
-    read_shader_file
+    read_shader_file,
 )
 
 from webgpu.webgpu_api import (
@@ -39,16 +38,18 @@ class Binding:
     NORMALS = 91
     INDICES = 92
 
+
 class IsoSurfaceRenderObject(RenderObject):
     def __init__(self, gpu, cf, mesh, label):
         super().__init__(gpu, label=label)
         self.cf = cf
         self.mesh = mesh
+        self.n_cut_trigs = None
+        self.cut_trigs_set = False
         self.update()
 
     def count_cut_trigs(self):
-        encoder = self.gpu.device.createCommandEncoder(
-            label="count_iso_trigs")
+        encoder = self.gpu.device.createCommandEncoder(label="count_iso_trigs")
         # binding -> counter i32
         print("mesh tets = ", self.mesh.ne)
         counter_buffer = self.device.createBuffer(
@@ -59,29 +60,32 @@ class IsoSurfaceRenderObject(RenderObject):
             size=4,
             usage=BufferUsage.MAP_READ | BufferUsage.COPY_DST,
         )
-        self.device.queue.writeBuffer(counter_buffer,
-                                      0, np.array([0], dtype=np.int32).tobytes(), 0, 4)
-        binding = BufferBinding(80,
-                                counter_buffer,
-                                read_only=False,
-                                visibility=ShaderStage.COMPUTE)
+        self.device.queue.writeBuffer(
+            counter_buffer, 0, np.array([0], dtype=np.int32).tobytes(), 0, 4
+        )
+        binding = BufferBinding(
+            80, counter_buffer, read_only=False, visibility=ShaderStage.COMPUTE
+        )
         layout = self.device.createBindGroupLayout(
-            entries=[BindGroupLayoutEntry(**binding.layout)
-            ], label="count_iso_trigs")
-        group = self.device.createBindGroup(label="count_iso_trigs",
-                                            layout=layout,
-                                            entries=[BindGroupEntry(**binding.binding)])
+            entries=[BindGroupLayoutEntry(**binding.layout)], label="count_iso_trigs"
+        )
+        group = self.device.createBindGroup(
+            label="count_iso_trigs",
+            layout=layout,
+            entries=[BindGroupEntry(**binding.binding)],
+        )
         print("layout = ", layout)
         shader_code = ""
+        # shader_code += read_shader_file("mesh.wgsl", __file__)
         shader_code += read_shader_file("isosurface.wgsl", __file__)
         shader_module = self.device.createShaderModule(shader_code)
         pipeline = self.device.createComputePipeline(
             self.device.createPipelineLayout([layout], self.label),
             label="count_iso_trigs",
             compute=ComputeState(
-                module=shader_module,
-                entryPoint="count_iso_triangles")
-            )
+                module=shader_module, entryPoint="count_iso_triangles"
+            ),
+        )
         # self.gpu.begin_render_pass(encoder, label="count_iso_trigs")
         compute_pass = encoder.beginComputePass(label="count_iso_trigs")
         compute_pass.setBindGroup(0, group)
@@ -89,40 +93,50 @@ class IsoSurfaceRenderObject(RenderObject):
         compute_pass.dispatchWorkgroups(self.mesh.ne)
         compute_pass.end()
 
-        counter_buffer.handle = counter_buffer
-        result_buffer.handle = result_buffer
         encoder.copyBufferToBuffer(counter_buffer, 0, result_buffer, 0, 4)
         self.device.queue.submit([encoder.finish()])
-        result = result_buffer.mapAsync(MapMode.READ)
-        def print_result(r):
-            data = result_buffer.getMappedRange(0, 4)
-            # b = np.frombuffer(data.to_bytes(), dtype=np.int32)
-            b = np.frombuffer(memoryview(data.to_py()), dtype=np.int32)
-            print("count = ", b)
-            result_buffer.unmap()
-        result.then(print_result)
+        import pyodide
 
-        
-        
-        
+        task = pyodide.webloop.asyncio.get_running_loop().run_until_complete(
+            result_buffer.mapAsync(MapMode.READ, 0, 4)
+        )
+
+        def read_buffer(task):
+            print("in read buffer")
+            data = result_buffer.getMappedRange(0, 4)
+            print("data = ", data)
+            b = np.frombuffer(memoryview(data.to_py()), dtype=np.int32)
+            print("b = ", b)
+            self.n_cut_trigs = b[0]
+            result_buffer.unmap()
+            self.write_cut_trigs()
+
+        task.then(read_buffer)
+
+    def write_cut_trigs(self):
+        pass
 
     def update(self):
         self.count_cut_trigs()
 
     def render(self, encoder):
-        pass
+        if self.cut_trigs_set is False:
+            return
 
 
 def render_isosurface(cf, mesh, name="isosurface"):
     from webgpu.jupyter import gpu
     import js
     import pyodide.ffi
+
     iso = IsoSurfaceRenderObject(gpu, cf, mesh, name)
+
     def render_function(t):
         gpu.update_uniforms()
         encoder = gpu.device.createCommandEncoder()
         iso.render(encoder)
         gpu.device.queue.submit([encoder.finish()])
+
     render_function = pyodide.ffi.create_proxy(render_function)
     gpu.input_handler.render_function = render_function
     js.requestAnimationFrame(render_function)
--- ngsolve_webgpu/shaders/isosurface.wgsl
+++ ngsolve_webgpu/shaders/isosurface.wgsl
@@ -1,7 +1,13 @@
 
-@group(0) @binding(80) var<storage, read_write> counter: atomic<i32>;
+@group(0) @binding(80) var<storage, read_write> counter_isosurface: atomic<i32>;
 
 @compute @workgroup_size(1)
 fn count_iso_triangles(@builtin(global_invocation_id) id: vec3<u32>) {
-  atomicAdd(&counter, 1);
+  atomicAdd(&counter_isosurface, 1);
+}
+
+@compute @workgroup_size(1)
+fn write_iso_triangles(@builtin(global_invocation_id) id: vec3<u32>)
+{
+
 }
