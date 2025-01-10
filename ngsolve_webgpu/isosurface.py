61dfddfb97f787c414f0e891aad32463be25b35f
Christopher Lackner <clackner@cerbsim.com>
2025-01-10 14:26:31 +0100

    isosurfaces with multiple compute passes (not working yet)

--- ngsolve_webgpu/isosurface.py
+++ ngsolve_webgpu/isosurface.py
@@ -2,6 +2,7 @@ import webgpu
 from webgpu.gpu import RenderObject
 from webgpu.utils import (
     BufferBinding,
+    UniformBinding,
     ShaderStage,
     TextureBinding,
     create_bind_group,
@@ -48,53 +49,94 @@ class IsoSurfaceRenderObject(RenderObject):
         self.cut_trigs_set = False
         self.update()
 
-    def count_cut_trigs(self):
-        encoder = self.gpu.device.createCommandEncoder(label="count_iso_trigs")
+    def create_cut_trigs(self, count=None):
+        import ngsolve as ngs
+        compute_encoder = self.gpu.device.createCommandEncoder(label="count_iso_trigs")
         # binding -> counter i32
         print("mesh tets = ", self.mesh.ne)
+        print("create cut trigs count=", count)
         counter_buffer = self.device.createBuffer(
             size=4,
             usage=BufferUsage.STORAGE | BufferUsage.COPY_SRC | BufferUsage.COPY_DST,
+            label="counter"
         )
-        result_buffer = self.device.createBuffer(
-            size=4,
-            usage=BufferUsage.MAP_READ | BufferUsage.COPY_DST,
+        mesh_pts = self.mesh.MapToAllElements(ngs.IntegrationRule([(0,0,0),
+                                                                   (1,0,0),
+                                                                   (0,1,0),
+                                                                   (0,0,1)]),
+                                              self.mesh.Materials(".*"))
+        func_values = self.cf(mesh_pts).flatten()
+        function_value_buffer = self.device.createBuffer(
+            size=len(func_values) * func_values.itemsize,
+            usage=BufferUsage.STORAGE | BufferUsage.COPY_DST,
+            label="function"
         )
         self.device.queue.writeBuffer(
-            counter_buffer, 0, np.array([0], dtype=np.int32).tobytes(), 0, 4
+            function_value_buffer, 0, func_values.tobytes())
+        vertices = ngs.CF((ngs.x,ngs.y,ngs.z))(mesh_pts).flatten()
+        vertex_buffer = self.device.createBuffer(
+            size=len(vertices) * vertices.itemsize,
+            label="vertex",
+            usage=BufferUsage.STORAGE | BufferUsage.COPY_DST)
+        self.device.queue.writeBuffer(
+            vertex_buffer, 0, vertices.tobytes())
+        result_buffer = self.device.createBuffer(
+            size=4, label="result",
+            usage=BufferUsage.MAP_READ | BufferUsage.COPY_DST,
         )
-        binding = BufferBinding(
-            80, counter_buffer, read_only=False, visibility=ShaderStage.COMPUTE
+        only_count = self.device.createBuffer(
+            label="only_count",
+            size=4, usage=BufferUsage.UNIFORM | BufferUsage.COPY_DST)
+        self.device.queue.writeBuffer(only_count,
+                                      0, np.array([1 if count is None else 0], dtype=np.uint32).tobytes(), 0, 4)
+        self.device.queue.writeBuffer(
+            counter_buffer, 0, np.array([0], dtype=np.uint32).tobytes(), 0, 4
         )
-        layout = self.device.createBindGroupLayout(
-            entries=[BindGroupLayoutEntry(**binding.layout)], label="count_iso_trigs"
-        )
-        group = self.device.createBindGroup(
-            label="count_iso_trigs",
-            layout=layout,
-            entries=[BindGroupEntry(**binding.binding)],
-        )
-        print("layout = ", layout)
+        bindings = [*self.gpu.u_view.get_bindings(),
+                    *self.gpu.u_font.get_bindings(),
+                    *self.gpu.u_mesh.get_bindings(),
+                    BufferBinding(80, counter_buffer,
+                          read_only=False,
+                          visibility=ShaderStage.COMPUTE),
+            BufferBinding(83, function_value_buffer,
+                          visibility=ShaderStage.ALL),
+            BufferBinding(12, vertex_buffer,
+                          visibility=ShaderStage.ALL),
+            UniformBinding(81, only_count)]
+        cut_buffer_size = 64 * (count if count is not None else 1)
+        print("cut_buffer_size = ", cut_buffer_size)
+        cut_trigs_buffer = self.device.createBuffer(
+            label="cut trigs",
+            size=cut_buffer_size,
+            usage=BufferUsage.STORAGE | BufferUsage.COPY_SRC)
+        cut_trigs_binding = BufferBinding(82, cut_trigs_buffer,
+                                          read_only=False,
+                                          visibility=ShaderStage.COMPUTE)
+
+        layout, group = create_bind_group(
+            self.device,
+            bindings + [cut_trigs_binding])
         shader_code = ""
+        compute_shader_code = read_shader_file("compute_isosurface.wgsl", __file__)
         # shader_code += read_shader_file("mesh.wgsl", __file__)
         shader_code += read_shader_file("isosurface.wgsl", __file__)
-        shader_module = self.device.createShaderModule(shader_code)
+        shader_module = self.device.createShaderModule(compute_shader_code + shader_code)
         pipeline = self.device.createComputePipeline(
             self.device.createPipelineLayout([layout], self.label),
             label="count_iso_trigs",
             compute=ComputeState(
-                module=shader_module, entryPoint="count_iso_triangles"
+                module=shader_module, entryPoint="create_iso_triangles"
             ),
         )
         # self.gpu.begin_render_pass(encoder, label="count_iso_trigs")
-        compute_pass = encoder.beginComputePass(label="count_iso_trigs")
+        compute_pass = compute_encoder.beginComputePass(label="count_iso_trigs")
         compute_pass.setBindGroup(0, group)
         compute_pass.setPipeline(pipeline)
         compute_pass.dispatchWorkgroups(self.mesh.ne)
         compute_pass.end()
 
-        encoder.copyBufferToBuffer(counter_buffer, 0, result_buffer, 0, 4)
-        self.device.queue.submit([encoder.finish()])
+        compute_encoder.copyBufferToBuffer(counter_buffer, 0, result_buffer, 0, 4)
+        self.device.queue.submit([compute_encoder.finish()])
         import pyodide
 
         task = pyodide.webloop.asyncio.get_running_loop().run_until_complete(
@@ -102,26 +144,63 @@ class IsoSurfaceRenderObject(RenderObject):
         )
 
         def read_buffer(task):
-            print("in read buffer")
             data = result_buffer.getMappedRange(0, 4)
-            print("data = ", data)
             b = np.frombuffer(memoryview(data.to_py()), dtype=np.int32)
             print("b = ", b)
             self.n_cut_trigs = b[0]
             result_buffer.unmap()
-            self.write_cut_trigs()
+            self.create_cut_trigs(b[0])
 
-        task.then(read_buffer)
+        if self.n_cut_trigs is None:
+            task.then(read_buffer)
+        else:
+            print("Create render pipeline with cut trigs set = ", self.n_cut_trigs)
+            # encoder = self.gpu.device.createCommandEncoder()
+            render_cut_trigs_binding = BufferBinding(82, cut_trigs_buffer,
+                                                     visibility=ShaderStage.VERTEX)
+            render_layout, self._bind_group = create_bind_group(
+                self.device,
+                bindings + [render_cut_trigs_binding])
+            self.n_cut_trigs = count
+            render_shader_code = read_shader_file("render_isosurface.wgsl", __file__)
+            render_shader_module = self.device.createShaderModule(render_shader_code + shader_code)
+            self._pipeline = self.device.createRenderPipeline(
+                self.device.createPipelineLayout([render_layout], self.label),
+                vertex=VertexState(module=render_shader_module,
+                                   entryPoint="vertexIsoSurface"),
+                fragment=FragmentState(module=render_shader_module,
+                                       entryPoint="fragmentIsoSurface",
+                                       targets=[
+                                           ColorTargetState(format=self.gpu.format)]),
+                primitive=PrimitiveState(
+                    topology=PrimitiveTopology.triangle_list),
+                depthStencil = DepthStencilState(
+                    format=TextureFormat.depth24plus,
+                    depthWriteEnabled=True,
+                    depthCompare=CompareFunction.less,
+                    # shift trigs behind to ensure that edges are rendered properly
+                    depthBias=1,
+                    depthBiasSlopeScale=1.0))
 
-    def write_cut_trigs(self):
-        pass
+            self.cut_trigs_set = True
+            # self.render(encoder)
+            # self.gpu.update_uniforms()
+            # self.gpu.device.queue.submit([encoder.finish()])
 
     def update(self):
-        self.count_cut_trigs()
+        self.create_cut_trigs()
 
     def render(self, encoder):
         if self.cut_trigs_set is False:
             return
+        print("render with cut trigs set = ", self.n_cut_trigs)
+        render_pass = self.gpu.begin_render_pass(encoder, label=self.label)
+        render_pass.setBindGroup(0, self._bind_group)
+        render_pass.setPipeline(self._pipeline)
+        # render_pass.draw(3, self.n_cut_trigs)
+        render_pass.draw(3, 1)
+        render_pass.end()
+
 
 
 def render_isosurface(cf, mesh, name="isosurface"):
@@ -139,4 +218,4 @@ def render_isosurface(cf, mesh, name="isosurface"):
 
     render_function = pyodide.ffi.create_proxy(render_function)
     gpu.input_handler.render_function = render_function
-    js.requestAnimationFrame(render_function)
+    # js.requestAnimationFrame(render_function)
--- ngsolve_webgpu/shaders/isosurface.wgsl
+++ ngsolve_webgpu/shaders/isosurface.wgsl
@@ -1,13 +1,109 @@
 
-@group(0) @binding(80) var<storage, read_write> counter_isosurface: atomic<i32>;
+@group(0) @binding(0) var<uniform> u_view : ViewUniforms;
+@group(0) @binding(80) var<storage, read_write> counter_isosurface: atomic<u32>;
+@group(0) @binding(81) var<uniform> only_count: u32;
+@group(0) @binding(12) var<storage> vertices : array<f32>;
+@group(0) @binding(83) var<storage> function_values: array<f32>;
 
-@compute @workgroup_size(1)
-fn count_iso_triangles(@builtin(global_invocation_id) id: vec3<u32>) {
-  atomicAdd(&counter_isosurface, 1);
+struct ViewUniforms {
+  model_view: mat4x4<f32>,
+  model_view_projection: mat4x4<f32>,
+  normal_mat: mat4x4<f32>,
+  aspect: f32,
+
+  padding0: u32,
+  padding1: u32,
+  padding2: u32,
+};
+
+
+struct SubTrig {
+  lam: array<vec3f, 3>,
+  id: u32,
 }
 
-@compute @workgroup_size(1)
-fn write_iso_triangles(@builtin(global_invocation_id) id: vec3<u32>)
+struct ClipTetResult {
+  n: u32,
+  trigs: array<SubTrig, 2>,
+}
+
+// clip tet such that the clip triangle(s) have value 0 everywhere
+fn clipTet(lam: array<vec3f, 4>, values: array<f32, 4>) -> ClipTetResult {
+    let ei = 0u;
+    var trigs = ClipTetResult(0, array<SubTrig, 2>(SubTrig(array<vec3f, 3>(vec3f(0.0), vec3f(0.0), vec3f(0.0)), 0), SubTrig(array<vec3f, 3>(vec3f(0.0), vec3f(0.0), vec3f(0.0)), 0)));
+    var p_pos = array<u32, 4>(0u, 0u, 0u, 0u);
+    var p_neg = array<u32, 4>(0u, 0u, 0u, 0u);
+
+    var n_pos: u32 = 0u;
+    var n_neg: u32 = 0u;
+
+    for (var i = 0u; i < 4u; i++) {
+        if values[i] > 0.0 {
+            p_pos[n_pos] = i;
+            n_pos++;
+        } else {
+            p_neg[n_neg] = i;
+            n_neg++;
+        }
+    }
+
+    if n_pos == 4u || n_neg == 4u {
+        return trigs;
+    }
+
+    if n_pos == 3u {
+        trigs.n = 1u;
+        n_pos = 1u;
+        n_neg = 3u;
+        p_pos[3] = p_neg[0];
+        p_neg[0] = p_pos[0];
+        p_neg[1] = p_pos[1];
+        p_neg[2] = p_pos[2];
+        p_pos[0] = p_pos[3];
+    }
+
+    if n_pos == 1u {
+        trigs.n = 1u;
+        trigs.trigs[0].id = ei;
+        for (var i = 0u; i < 3u; i++) {
+            let t = values[p_pos[0] ] / (values[p_pos[0] ] - values[p_neg[i] ]);
+            let lam_trig = mix(lam[p_pos[0] ], lam[p_neg[i] ], t);
+            trigs.trigs[0].lam[i] = lam_trig;
+        }
+        return trigs;
+    }
+
+  // two points before, two points behind clipping plane
+  // -> two triangles
+    trigs.n = 2u;
+    var pairs = array<vec2u,4>(
+        vec2u(p_pos[1], p_neg[0]),
+        vec2u(p_pos[0], p_neg[0]),
+        vec2u(p_pos[0], p_neg[1]),
+        vec2u(p_pos[1], p_neg[1])
+    );
+    var points: array<vec3f, 4> = array<vec3f, 4>(vec3f(0.0), vec3f(0.0), vec3f(0.0), vec3f(0.0));
+    for (var i = 0; i < 4; i++) {
+        let p0 = pairs[i].x;
+        let p1 = pairs[i].y;
+        let t = values[p0 ] / (values[p0] - values[p1]);
+        let lam_trig = mix(lam[p0], lam[p1], t);
+        points[i] = lam_trig;
+    }
+    trigs.trigs[0].id = ei;
+    trigs.trigs[0].lam = array(points[0], points[1], points[2]);
+    trigs.trigs[1].id = ei;
+    trigs.trigs[1].lam = array(points[0], points[2], points[3]);
+    return trigs;
+}
+
+fn get_tet_points(elid: u32) -> array<vec3f, 4>
 {
-
+  var p: array<vec3f, 4>;
+  for (var i = 0; i < 4; i++) {
+    p[i] = vec3<f32>(vertices[3*elid], vertices[3*elid + 1], vertices[3*elid + 2]);
+  }
+  return p;
 }
+
+
