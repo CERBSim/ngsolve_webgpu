2cd2624f3f49d5aeaa2acbc96044c120d381d948
Matthias Hochsteger <mhochsteger@cerbsim.com>
2025-02-13 19:52:04 +0100

    Load ngsolve from ngsolve.org

--- ngsolve_webgpu/jupyter.py
+++ ngsolve_webgpu/jupyter.py
@@ -1,26 +1,53 @@
 import ngsolve as ngs
 import webgpu.jupyter as wj
 
-# local_path = None # change this to local path of pyodide compiled zip files
-local_path = "/home/christopher/git/source/ngsolve/dist/pyodide"
+_local_path = None  # change this to local path of pyodide compiled zip files
 
-if local_path is None:
-    wj.pyodide_install_packages(["ngsolve"])
+if _local_path is None:
+    wj.run_code_in_pyodide(
+        """
+    _NGSOLVE_BASE_URL = "https://ngsolve.org/files/pyodide-0.27.2/master/"
+
+    import micropip
+    from pyodide.http import pyfetch
+    import pyodide_js
+    from pyodide._package_loader import unpack_buffer
+
+    await micropip.install("scipy")
+    import scipy
+
+    for module in ["pyngcore", "netgen", "ngsolve"]:
+        response = await pyfetch(f"{_NGSOLVE_BASE_URL}/{module}.zip")
+        data = await response.buffer()
+        dynlibs = list(
+            unpack_buffer(
+                data, format="zip", filename=f"{module}.zip", calculate_dynlibs=True
+            )
+        )
+        for lib in dynlibs:
+            await pyodide_js._api.loadDynlib(lib, True, [])
+    """
+    )
 else:
+
     def local_install(local_packages):
         packages = []
         for package in local_packages:
-            with open(local_path + f"/{package}.zip", "rb") as f:
+            with open(_local_path + f"/{package}.zip", "rb") as f:
                 data = f.read()
             packages.append((package, data))
         packages = wj._encode_data(packages)
         print("packages = ", packages)
-        wj.run_code_in_pyodide(f"""
+        wj.run_code_in_pyodide(
+            f"""
 import shutil
 from pyodide._package_loader import get_dynlibs
-import pyodide_js        
+import pyodide_js
 from pathlib import Path
 import webgpu.jupyter as wj
+import micropip
+await micropip.install('scipy')
+import scipy
 for package, data in wj._decode_data('{packages}'):
     with open(package + '.zip', 'wb') as f:
             f.write(data)
@@ -35,16 +62,11 @@ for package, data in wj._decode_data('{packages}'):
     import importlib
     print('import package = ', package)
     importlib.import_module(package)
-""")
+"""
+        )
+
     local_install(["pyngcore", "netgen", "ngsolve"])
-    # wj.run_code_in_pyodide("""import netgen
-# print('netgen', netgen.__file__)""")
-#     wj.run_code_in_pyodide("""
-# import micropip
-# await micropip.install('scipy')    
-# import scipy""")
-#     wj.run_code_in_pyodide("""import ngsolve
-# print('ngsolve', ngsolve.__file__)""")
+
 
 def Draw(
     obj: ngs.CoefficientFunction | ngs.Mesh,
@@ -84,7 +106,7 @@ def Draw(
                 mesh = mesh.space.mesh
             else:
                 raise ValueError("If obj is a CoefficientFunction, mesh is required.")
-        from .mesh import CoefficientFunctionRenderObject, MeshData, FunctionData
+        from .mesh import CoefficientFunctionRenderObject, FunctionData, MeshData
 
         mesh_data = MeshData(mesh.ngmesh)
         function_data = FunctionData(mesh_data, obj, order)
