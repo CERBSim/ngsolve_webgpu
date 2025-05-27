94cef249400dd48934078d69653b213cd73db055
Christopher Lackner <clackner@cerbsim.com>
2025-05-27 14:23:51 +0200

    default imports with from ngsolve_webgpu import *

--- ngsolve_webgpu/__init__.py
+++ ngsolve_webgpu/__init__.py
@@ -1,4 +1,10 @@
-from webgpu.utils import register_shader_directory
-from pathlib import Path
 
-register_shader_directory("ngsolve", Path(__file__).parent / "shaders")
+from .mesh import MeshData, MeshWireframe2d, MeshElements2d, MeshElements3d
+from .cf import FunctionData, CFRenderer
+from webgpu.colormap import Colorbar
+
+
+from webgpu.utils import register_shader_directory as _register_shader_directory
+from pathlib import Path as _Path
+
+_register_shader_directory("ngsolve", _Path(__file__).parent / "shaders")
